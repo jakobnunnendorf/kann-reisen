ffd2b3ad1a5f0c644a3f36bf058a1a497654efef
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-07-02 14:41:12 +0800

    move images to public folder

--- components/Desktop/Banner/BrandDisplay/BigLogo/BigLogo.tsx
+++ components/Desktop/Banner/BrandDisplay/BigLogo/BigLogo.tsx
@@ -6,7 +6,7 @@ import "./biglogo.css";
 const BigLogo = () => {
   return (
     <div className="logo-section bigLogo">
-      <img src="BigLogo.png" alt="" className="banner-logo" />
+      <img src="/BigLogo.png" alt="" className="banner-logo" />
       <h3>Urlaub, im Paradies</h3>
     </div>
   );
--- components/Desktop/Gallery/Gallery.tsx
+++ components/Desktop/Gallery/Gallery.tsx
@@ -2,48 +2,28 @@
 
 import React, { useState, useEffect } from "react";
 import "./Gallery.css";
-import bahnhof from "./bilder/bahnhof.jpeg";
-import brandenburgertor from "./bilder/brandenburgertor.jpeg";
-import chicago from "./bilder/chicago.jpeg";
-import eifelturm from "./bilder/eifelturm.jpeg";
-import fluss from "./bilder/fluss.jpeg";
-import gasse from "./bilder/gasse.jpeg";
-import italy from "./bilder/italy.jpeg";
-import markt from "./bilder/markt.jpeg";
-import rom from "./bilder/rom.jpeg";
-import russland from "./bilder/russland.jpeg";
-import santorini1 from "./bilder/santorini1.jpeg";
-import santorini2 from "./bilder/santorini2.jpeg";
-import sidney from "./bilder/sidney.jpeg";
-import strasse from "./bilder/strasse.jpeg";
-import toscana from "./bilder/toscana.jpeg";
-import verona from "./bilder/verona.jpeg";
-import weihnachten from "./bilder/weihnachten.jpeg";
-
 const images = [
-  bahnhof,
-  brandenburgertor,
-  chicago,
-  eifelturm,
-  fluss,
-  gasse,
-  italy,
-  markt,
-  rom,
-  russland,
-  santorini1,
-  santorini2,
-  sidney,
-  strasse,
-  toscana,
-  verona,
-  weihnachten,
+  "/images/gallery/bahnhof.jpeg",
+  "/images/gallery/brandenburgertor.jpeg",
+  "/images/gallery/chicago.jpeg",
+  "/images/gallery/eifelturm.jpeg",
+  "/images/gallery/fluss.jpeg",
+  "/images/gallery/gasse.jpeg",
+  "/images/gallery/italy.jpeg",
+  "/images/gallery/markt.jpeg",
+  "/images/gallery/rom.jpeg",
+  "/images/gallery/russland.jpeg",
+  "/images/gallery/santorini1.jpeg",
+  "/images/gallery/santorini2.jpeg",
+  "/images/gallery/sidney.jpeg",
+  "/images/gallery/strasse.jpeg",
+  "/images/gallery/toscana.jpeg",
+  "/images/gallery/verona.jpeg",
+  "/images/gallery/weihnachten.jpeg",
 ];
 
 const Gallery: React.FC = () => {
-  const [currentImage, setCurrentImage] = useState<(typeof images)[0]>(
-    images[0]
-  );
+  const [currentImage, setCurrentImage] = useState<string>(images[0]);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * images.length);
@@ -54,7 +34,7 @@ const Gallery: React.FC = () => {
     <div className="gallery" id="gallery">
       <h3>Ein Fenster in andere Welten</h3>
       <div className="vacay-window">
-        <img src={currentImage.src} alt="" />
+        <img src={currentImage} alt="" />
       </div>
     </div>
   );
