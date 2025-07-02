093b8ae16d712720b382211afc7ed3427e3d6c19
Jakob Nunnendorf <Jnunnendorf@icloud.com>
2025-07-02 16:46:21 +0800

    fix Info component

--- components/Desktop/AboutUs/components/contact-section/Info.jsx
+++ components/Desktop/AboutUs/components/contact-section/Info.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import { Icon, InlineIcon } from "@iconify/react";
-
+import { InlineIcon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker-radius-outline";
 import phoneIcon from "@iconify/icons-mdi/phone-outline";
 import emailIcon from "@iconify/icons-mdi/email-outline";
-
-import facebookIcon from "@iconify/icons-mdi/facebook";
-import linkedinIcon from "@iconify/icons-mdi/linkedin";
-import twitterIcon from "@iconify/icons-mdi/twitter";
-
+import Image from "next/image";
 import "./info.css";
 
 const contactDetails = [
@@ -20,23 +15,20 @@ const contactDetails = [
   { value: "info@kannreisen.com", icon: emailIcon },
 ];
 
-const renderContactDetails = () =>
-  contactDetails.map((detail) => (
-    <p key={detail.value} className="info-detail">
-      <InlineIcon icon={detail.icon} /> {detail.value}
-    </p>
-  ));
-
-const renderIcons = () =>
-  [facebookIcon, linkedinIcon, twitterIcon].map((icon, key) => (
-    <Icon icon={icon} key={key} className="info-icon" />
-  ));
-
 const Info = () => (
   <section className="info">
-    <h2 className="info-h2">So erreichen Sie uns</h2>
-
-    <div className="info-details-container">{renderContactDetails()}</div>
+    <h2 className="uppercase text-2xl">So erreichen Sie uns</h2>
+    <div className="h-48 w-96 relative flex justify-start">
+      <Image src="/logo.png" alt="Contact" fill className="object-contain" />
+    </div>
+    <ul className="info-details-container">
+      {contactDetails.map((detail) => (
+        <li key={detail.value} className="info-detail flex items-center gap-2">
+          <InlineIcon icon={detail.icon} />
+          <p>{detail.value}</p>
+        </li>
+      ))}
+    </ul>
   </section>
 );
 
